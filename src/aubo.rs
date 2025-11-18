32ce5627fd47d0ca511a53d359d72e24d304af50
yixing <yanjizhou312@163.com>
2025-11-18 15:38:06 +0800

    fix: aubo folder

---
+++ src/aubo.rs
@@ -0,0 +1,11 @@
+mod aubo_c;
+mod aubo_e;
+mod aubo_i;
+mod aubo_ih;
+mod aubo_is;
+
+pub use aubo_c::*;
+pub use aubo_e::*;
+pub use aubo_i::*;
+pub use aubo_ih::*;
+pub use aubo_is::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,7 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
-pub mod aubo_c;
-pub mod aubo_e;
-pub mod aubo_i;
-pub mod aubo_ih;
-pub mod aubo_is;
+pub mod aubo;
 pub mod robot;
 
-pub use aubo_c::*;
-pub use aubo_e::*;
-pub use aubo_i::*;
-pub use aubo_ih::*;
-pub use aubo_is::*;
+pub use aubo::*;
 pub use robot::*;
